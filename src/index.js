9e1eb0401026c156f4f3cbb90f1014c242a77b8b
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-09-24 10:50:31 -0400

    Simple solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskDescriptionElement = event.target.querySelector("#new-task-description");
+
+    const newTaskDiv = document.createElement("div");
+
+    const newTaskDeleteBtn = document.createElement("button");
+    newTaskDeleteBtn.textContent = "X";
+    newTaskDeleteBtn.addEventListener("click", (event) => {
+      event.target.parentElement.remove();
+    });
+
+    newTaskDiv.appendChild(newTaskDeleteBtn);
+
+    const newTaskDescription = document.createElement("span");
+    newTaskDescription.textContent = taskDescriptionElement.value;
+    newTaskDiv.appendChild(newTaskDescription);
+
+    const taskList = document.querySelector("#tasks");
+    taskList.appendChild(newTaskDiv);
+
+    taskDescriptionElement.value = "";
+  });
 });
+
