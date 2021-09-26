2e709647383a5167215ccbc46053cc6002f21cfa
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-09-25 22:40:57 -0400

    Stretch deliverable progress

--- src/index.js
+++ src/index.js
@@ -1,40 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById('create-task-form');
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const newTaskRow = document.createElement("tr");
-
-    const taskDescriptionElement = event.target.querySelector("#new-task-description");
-    const newTaskDescription = createTableCell('description', taskDescriptionElement.value);
-    newTaskRow.appendChild(newTaskDescription);
-
-    const taskDueDateElement = event.target.querySelector("#new-task-duedate");
-    const newTaskDueDate = createTableCell('due-date', taskDueDateElement.value);
-    newTaskRow.appendChild(newTaskDueDate);
-
-    const taskPriorityElement = event.target.querySelector("#new-task-priority");
-    const newTaskPriority = createTableCell('priority', taskPriorityElement.value);
-    newTaskRow.appendChild(newTaskPriority);
-
-    const newTaskDelete = createTableCell('delete');
-    const newTaskDeleteBtn = document.createElement("button");
-    newTaskDeleteBtn.textContent = "X";
-    newTaskDeleteBtn.addEventListener("click", (event) => {
-      // event => button => td => tr
-      event.target.parentElement.parentElement.remove();
-    });
-    newTaskDelete.appendChild(newTaskDeleteBtn);
-    newTaskRow.appendChild(newTaskDelete);
-
-    const taskList = document.querySelector("#tasks tbody");
-    taskList.appendChild(newTaskRow);
-
-    taskDescriptionElement.value = "";
-    taskDueDateElement.value = "";
-    taskPriorityElement.value = "Low";
-  });
+  form.addEventListener('submit', handleAddTask);
 });
+
+function handleAddTask(event) {
+  event.preventDefault();
+  let form = event.target;
+
+  const newTaskRow = document.createElement("tr");
+
+  const newTaskData = [
+    {
+      inputId: "#new-task-description",
+      cellClassName: "description"
+    },
+    {
+      inputId: "#new-task-duedate",
+      cellClassName: "due-date"
+    },
+    {
+      inputId: "#new-task-priority",
+      cellClassName: "priority",
+      defaultInputValue: "Low"
+    }
+  ];
+  newTaskData.forEach(cellData => {
+    const dataInputElement = form.querySelector(cellData.inputId);
+    const newCell = createTableCell(cellData.cellClassName, dataInputElement.value);
+    newTaskRow.appendChild(newCell);
+    dataInputElement.value = cellData.defaultInputValue ? cellData.defaultInputValue : "";
+  });
+
+  const newTaskEdit = createTableCell('edit');
+  const newTaskEditLink = createEditLink();
+  newTaskEdit.append(newTaskEditLink);
+  newTaskRow.appendChild(newTaskEdit);
+
+  const newTaskDelete = createTableCell('delete');
+  const newTaskDeleteBtn = document.createElement("button");
+  newTaskDeleteBtn.textContent = "X";
+  newTaskDeleteBtn.addEventListener("click", (event) => {
+    // event => button => td => tr
+    event.target.parentElement.parentElement.remove();
+  });
+  newTaskDelete.appendChild(newTaskDeleteBtn);
+  newTaskRow.appendChild(newTaskDelete);
+
+  const taskList = document.querySelector("#tasks tbody");
+  taskList.appendChild(newTaskRow);
+}
 
 function createTableCell(className='', textContent='') {
   let newCell = document.createElement('td');
@@ -48,11 +62,12 @@ function sortTasks(sortingProperty) {
   const taskRowsArray = Array.from(taskTableBody.querySelectorAll('tr'));
 
   taskRowsArray.sort((first, second) => {
+    // TODO: sorting doesn't work for items being edited
     let firstProperty = first.querySelector(`.${sortingProperty}`).textContent;
     let secondProperty = second.querySelector(`.${sortingProperty}`).textContent;
 
     if (sortingProperty === 'priority') {
-      const priorityObj = {'Low': 3, 'Medium': 2, 'High': 1};
+      const priorityObj = priorityOptions();
       firstProperty = priorityObj[firstProperty];
       secondProperty = priorityObj[secondProperty];
     }
@@ -68,5 +83,85 @@ function sortTasks(sortingProperty) {
 }
 
 function handleClickEdit(event) {
+  let rowToEdit = event.target.parentElement.parentElement;
 
+  let taskDescriptionCell = rowToEdit.querySelector('.description');
+  let taskDescriptionInput = createFormInput('text', taskDescriptionCell.innerText);
+  taskDescriptionCell.textContent = '';
+  taskDescriptionCell.append(taskDescriptionInput);
+
+  let taskDueDateCell = rowToEdit.querySelector('.due-date');
+  let taskDueDateInput = createFormInput('date', taskDueDateCell.innerText);
+  taskDueDateCell.textContent = '';
+  taskDueDateCell.append(taskDueDateInput);
+
+  let taskPriorityCell = rowToEdit.querySelector('.priority');
+  let taskPriorityInput = createPrioritySelector();
+  taskPriorityInput.value = taskPriorityCell.innerText;
+  taskPriorityCell.innerText = "";
+  taskPriorityCell.append(taskPriorityInput);
+
+  let taskEditCell = rowToEdit.querySelector('.edit');
+  let taskUpdateLink = createUpdateLink();
+  taskEditCell.firstChild.remove();
+  taskEditCell.append(taskUpdateLink);
+}
+
+function handleClickUpdate(event) {
+  let rowToEdit = event.target.parentElement.parentElement;
+
+  const inputCellClasses = ['description', 'due-date', 'priority'];
+  for (const cellClass of inputCellClasses) {
+    const cellToUpdate = rowToEdit.querySelector(`.${cellClass}`);
+    const newValue = cellToUpdate.firstChild.value;
+    cellToUpdate.innerText = newValue;
+
+    if (cellClass === 'priority') {
+      rowToEdit.className = `priority-${newValue.toLowerCase()}`;
+    }
+  }
+
+  let taskEditCell = rowToEdit.querySelector('.edit');
+  let taskEditLink = createEditLink();
+  taskEditCell.firstChild.remove();
+  taskEditCell.append(taskEditLink);
+}
+
+function createEditLink() {
+  let editLink = document.createElement('a');
+  editLink.textContent = 'Edit';
+  editLink.href = '#';
+  editLink.addEventListener('click', handleClickEdit);
+  return editLink;
+}
+
+function createUpdateLink() {
+  let updateLink = document.createElement('a');
+  updateLink.textContent = 'Update';
+  updateLink.href = '#';
+  updateLink.addEventListener('click', handleClickUpdate);
+  return updateLink;
+}
+
+function createFormInput(inputType, defaultValue) {
+  let newInput = document.createElement('input');
+  newInput.type = inputType;
+  newInput.value = defaultValue;
+  return newInput;
+}
+
+function createPrioritySelector() {
+  let newSelect = document.createElement('select');
+  let priorityObj = priorityOptions();
+  for (const priority in priorityObj) {
+    let newOption = document.createElement('option');
+    newOption.value = priority;
+    newOption.textContent = priority;
+    newSelect.append(newOption);
+  }
+  return newSelect;
+}
+
+function priorityOptions() {
+  return {'Low': 3, 'Medium': 2, 'High': 1};
 }
